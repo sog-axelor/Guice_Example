00ab485887ef956dfef3b699c584ea5f4a338f02
Sonu <s.gupta@axelor.com>
2023-01-12 18:45:12 +0530

    ConstantBindings

--- src/main/java/com/unique/app/GuiceTester.java
+++ src/main/java/com/unique/app/GuiceTester.java
@@ -1,10 +1,45 @@
 package com.unique.app;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+
 public class GuiceTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Injector ij = Guice.createInjector(new salaryModule());
+		salary sl = ij.getInstance(salary.class);
+		sl.emp();	
 	}
-
 }
+
+//1.create class
+class salary{
+	private String name;
+	
+	@Inject
+	public salary(@Named("Test") String name) {
+		this.name = name;
+	}
+	 
+	public void emp(){
+		System.out.println(name);
+	}
+}
+
+//2.create module
+
+class salaryModule extends AbstractModule{
+	
+	@Override
+	protected void configure() {
+		bind(String.class).annotatedWith(Names.named("Test")).toInstance("Hello akshay sir");
+	}
+}
+
+
+
