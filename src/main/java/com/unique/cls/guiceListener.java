f31f0bfdd3615a6397af28c99ba0c1262aa3c253
Sonu <s.gupta@axelor.com>
2023-01-16 13:54:58 +0530

    error with module

--- src/main/java/com/unique/cls/guiceListener.java
+++ src/main/java/com/unique/cls/guiceListener.java
@@ -5,13 +5,19 @@ import javax.servlet.annotation.WebListener;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
 
 @WebListener
 public class guiceListener extends GuiceServletContextListener{
 
 	@Override
 	protected Injector getInjector() {
-	    return Guice.createInjector(new guiceModule());
+	    return Guice.createInjector(new ServletModule(){
+	    	@Override
+	    	protected void configureServlets(){
+	    		serve("/").with(test.class);
+	    	}
+	    });
 	    
 	}
 
