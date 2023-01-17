a0d2abc1987d65ef94907a0c79ba394b5dab1166
Sonu <s.gupta@axelor.com>
2023-01-17 16:59:03 +0530

    method interceptor

--- src/main/java/com/unique/impl/salaryImpl.java
+++ src/main/java/com/unique/impl/salaryImpl.java
@@ -6,7 +6,7 @@ public class salaryImpl implements salaryCount {
 
 	@Override
 	public void gSalary(int s, int p, int e) {
-		int salary = s + (p - e);
+		int salary = s - (p + e);
 		System.out.println("The Intial salary is  : " + s);
 		System.out.println("The Gross salary is  : " + salary);
 	}
